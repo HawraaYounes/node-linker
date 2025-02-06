5090fbfcc3d47bef93f2bdac967734a26aca2b55
HawraaYounes <hawraayounes0@gmail.com>
2025-02-06 12:38:00 +0200

    aded more habits constants

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,6 +1,12 @@
-// constants/habits.ts
 export const habits = [
-    { value: "reading", label: "Reading" },
-    { value: "exercise", label: "Exercise" },
-    { value: "meditation", label: "Meditation" },
-  ];
+  { value: "reading", label: "Reading" },
+  { value: "exercise", label: "Exercise" },
+  { value: "meditation", label: "Meditation" },
+  { value: "journaling", label: "Journaling" },
+  { value: "hydration", label: "Hydration" },
+  { value: "sleeping", label: "Sleeping well" },
+  { value: "healthyEating", label: "Healthy Eating" },
+  { value: "learning", label: "Learning new skills" },
+  { value: "timeManagement", label: "Time Management" },
+  { value: "gratitude", label: "Gratitude practice" },
+];
