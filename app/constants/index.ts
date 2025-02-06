405e7b5c8c9e926b1c97c955cf446b72e9000e08
HawraaYounes <hawraayounes0@gmail.com>
2025-02-06 11:17:58 +0200

    added habits constants file

---
+++ app/constants/index.ts
@@ -0,0 +1,6 @@
+// constants/habits.ts
+export const habits = [
+    { value: "reading", label: "Reading" },
+    { value: "exercise", label: "Exercise" },
+    { value: "meditation", label: "Meditation" },
+  ];
