f2f8a68f239b04a27b717ffa6b73e6c8a54e2272
HawraaYounes <hawraayounes0@gmail.com>
2025-02-07 21:50:01 +0200

    added usegraph

--- app/hooks/useGraph.ts
+++ app/hooks/useGraph.ts
@@ -1,13 +1,30 @@
 "use client";
-import { useState } from 'react';
-import {
-  Node,
-} from 'reactflow';
+
+import { useEffect, useState } from "react";
+import { Node, Edge } from "reactflow";
+import "reactflow/dist/style.css";
+import { fetchEdges, fetchNodes, addNode } from "../actions/graphActions"; // Import addNode
 
 export default function useGraph() {
   const [nodes, setNodes] = useState<Node[]>([]);
+  const [edges, setEdges] = useState<Edge[]>([]);
 
-  return {
-    nodes,
+  useEffect(() => {
+    async function loadNodesAndEdges() {
+      const nodesData = await fetchNodes();
+      const edgesData = await fetchEdges();
+      setNodes(nodesData);
+      setEdges(edgesData);
+    }
+
+    loadNodesAndEdges();
+  }, []);
+
+  // Handle adding a new node
+  const handleAddNode = async (newNode: Node) => {
+    await addNode(newNode); // Add the node to the storage (or database)
+    setNodes([...nodes, newNode]); // Update the local state to re-render the nodes
   };
+
+  return { nodes, edges, handleAddNode };
 }
