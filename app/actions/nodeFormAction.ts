fad24118b7bec9f7cd7abced439710c1c318d465
HawraaYounes <hawraayounes0@gmail.com>
2025-02-07 13:11:32 +0200

    fixed nodeName typo

--- app/actions/nodeFormAction.ts
+++ app/actions/nodeFormAction.ts
@@ -2,21 +2,18 @@ import { NodeFormSchema } from "../zod/node-schema";
 
 
 export const nodeFormAction = async (_: any, formData: FormData) => {
-    console.log(",111111dmdndndn")
 
-  const nodeNamee = formData.get("nodeNamee") as string;
-  console.log(",,dmdndndn")
+    const nodeNamee = formData.get("nodeNamee") as string;
+    const validatedFields = NodeFormSchema.safeParse({ nodeNamee });
 
-  const validatedFields = NodeFormSchema.safeParse({ nodeNamee });
+    if (!validatedFields.success) {
+        return {
+            success: false,
+            message: validatedFields.error.flatten().fieldErrors as { nodeNamee?: string[] },
+            values: { nodeNamee }, // Preserve input values
+        };
+    }
 
-  if (!validatedFields.success) {
-    return {
-      success: false,
-      message: validatedFields.error.flatten().fieldErrors as { nodeNamee?: string[] },
-      values: { nodeNamee }, // Preserve input values
-    };
-  }
-
-  console.log("Form Submitted Successfully:", validatedFields.data);
-  return { success: true, message: {}, values: { nodeNamee: "" } }; // Ensure message exists
+    console.log("Form Submitted Successfully:", validatedFields.data);
+    return { success: true, message: {}, values: { nodeNamee: "" } }; // Ensure message exists
 };
